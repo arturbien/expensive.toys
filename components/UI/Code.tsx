aa6bf07dab762ef4a4cad811db5903c357f82adb
Artur Bien <artur.bien@gmail.com>
2023-04-06 19:31:38 +0200

    add 'Copy' button to Code

--- components/UI/Code.tsx
+++ components/UI/Code.tsx
@@ -1,23 +1,61 @@
 import React from "react";
-import { Frame } from "react95";
+import { Button, Frame } from "react95";
 import styled from "styled-components";
 
+const CopyButton = styled(Button)`
+  position: absolute;
+  right: 2px;
+  top: 2px;
+`;
+
+const Wrapper = styled.div`
+  width: 100%;
+  position: relative;
+  margin: 1rem 0;
+
+  ${CopyButton} {
+    visibility: hidden;
+  }
+  &:hover {
+    ${CopyButton} {
+      visibility: visible;
+    }
+  }
+`;
 const StyledCode = styled(Frame)`
   width: 100%;
   font-size: 12px;
   line-height: 1.5;
   overflow-x: auto;
   padding: 1rem;
-  margin: 1rem 0;
 `;
 
 const Code = (
   props: React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>
 ) => {
+  const codeRef = React.useRef<null | HTMLElement>(null);
+
+  const onCopy = () => {
+    if (codeRef.current) {
+      const selection = window.getSelection();
+      const range = document.createRange();
+      range.selectNodeContents(codeRef.current);
+      selection.removeAllRanges();
+      selection.addRange(range);
+      document.execCommand("copy");
+      selection.removeAllRanges();
+    }
+  };
+
   return (
-    <StyledCode variant="well">
-      <code {...props} />
-    </StyledCode>
+    <Wrapper>
+      <StyledCode variant="well">
+        <code {...props} ref={codeRef} />
+      </StyledCode>
+      <CopyButton variant="raised" onClick={onCopy}>
+        Copy
+      </CopyButton>
+    </Wrapper>
   );
 };
 
