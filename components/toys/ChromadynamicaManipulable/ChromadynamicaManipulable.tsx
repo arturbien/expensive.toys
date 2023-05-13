2c72a0a859986906e87d348a0eec2bcbfc43a506
Artur Bien <artur.bien@gmail.com>
2023-05-13 20:42:47 +0200

    WIP

--- components/toys/ChromadynamicaManipulable/ChromadynamicaManipulable.tsx
+++ components/toys/ChromadynamicaManipulable/ChromadynamicaManipulable.tsx
@@ -3,35 +3,31 @@ import styled from "styled-components";
 import useDragRotate from "./useDragRotate";
 import { Draggable } from "gsap/dist/Draggable";
 import gsap from "gsap";
+import useSound from "use-sound";
+import useResizeObserver from "use-resize-observer";
+import { serialize } from "v8";
 
 gsap.registerPlugin(Draggable);
 
 const Center = styled.div`
-  overflow: hidden;
-  contain: layout;
+  flex-shrink: 0;
+  contain: strict;
   isolation: isolate;
   width: auto;
   height: auto;
   display: grid;
   width: 100%;
   max-width: 500px;
+  width: 360px;
+  height: 360px;
   aspect-ratio: 1 / 1;
   align-content: stretch;
   justify-content: stretch;
 
-  /* &:after {
-    content: "";
-    position: absolute;
-    inset: 0;
-    filter: url(#noiseFilter);
-    mix-blend-mode: soft-light;
-  } */
-  svg {
-    display: none;
-  }
   filter: drop-shadow(6.8px 13.6px 5.6px hsl(0deg 0% 0% / 0.29));
 `;
-const Wrapper = styled.div<{ sizeRatio: number }>`
+const Wheel = styled.div<{ sizeRatio: number }>`
+  transform: rotate(0deg);
   grid-area: 1 / 1;
   align-self: center;
   justify-self: center;
@@ -46,6 +42,157 @@ const Wrapper = styled.div<{ sizeRatio: number }>`
   align-content: center;
   border-radius: 50%;
 
+  --border-width: 1px;
+  --edge-width: 4px;
+  border: var(--border-width) solid black;
+
+  &::before,
+  &:after {
+    content: "";
+    position: absolute;
+    z-index: 1;
+    inset: 0;
+    border-radius: inherit;
+  }
+  &:before {
+    background: linear-gradient(
+      to right bottom,
+      rgba(255, 255, 255, 0.35),
+      rgba(0, 0, 0, 0.45)
+    );
+    padding: 1px;
+    -webkit-mask: linear-gradient(black, black) content-box content-box,
+      linear-gradient(black, black);
+    -webkit-mask-composite: xor;
+    border-radius: inherit;
+  }
+  &:after {
+    inset: -2px;
+    background: linear-gradient(
+      to right bottom,
+      rgba(0, 0, 0, 0.45),
+      rgba(255, 255, 255, 0.35)
+    );
+    -webkit-mask: linear-gradient(black, black) content-box content-box,
+      linear-gradient(black, black);
+    -webkit-mask-composite: xor;
+    border-radius: inherit;
+    padding: 1px;
+  }
+  /* overflow: hidden; */
+`;
+
+const Background = styled.div<{ size?: number }>`
+  position: absolute;
+  inset: 0;
+  border-radius: inherit;
+  overflow: hidden;
+  contain: strict;
+  isolation: isolate;
+`;
+
+const W = styled.div<{ size: number }>`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  width: ${(p) => p.size + "px"};
+  height: ${(p) => p.size + "px"};
+  transform: translate(-50%, -50%);
+`;
+const Circle = styled.div<{ rotation?: number; color: string }>`
+  position: absolute;
+  width: 50%;
+  height: 50%;
+  border-radius: 999px;
+  top: 0;
+  left: 50%;
+  transform: rotate(${(p) => p.rotation || 0}deg) translate(-50%, 0);
+  transform-origin: 0 100%;
+  background: ${(p) => p.color};
+  filter: blur(30px);
+  mix-blend-mode: multiply;
+`;
+
+export const Gradient2 = ({
+  style,
+  ...otherProps
+}: React.ComponentProps<typeof Background>) => {
+  return (
+    <Background style={{ ...style, background: "white" }}>
+      <W {...otherProps}>
+        <Circle color="yellow" />
+        <Circle color="magenta" rotation={120} />
+        <Circle color="blue" rotation={180} />
+        {/* <Red />
+        <Blue /> */}
+      </W>
+    </Background>
+  );
+};
+export const Gradient3 = styled(Background)`
+  &::before {
+    content: "";
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    width: ${(p) => p.size + "px"};
+    height: ${(p) => p.size + "px"};
+    transform: translate(-50%, -50%);
+
+    background-image: url(https://img.freepik.com/free-photo/portrait-white-man-isolated_53876-40306.jpg);
+    background-size: 600px 600px;
+    background-position: center;
+    background-image: linear-gradient(
+      #010101,
+      #3a045e,
+      #2527ba,
+      #48a1f9,
+      #73f7fe,
+      #e2fde5,
+      #fcfffe,
+      #f8df5e,
+      #e84537,
+      #7e1858,
+      #010101,
+      #39047a,
+      #2e21d7,
+      #3f8ff6,
+      #acfd55,
+      #d5c240,
+      #ea4425,
+      #d12f2a,
+      #6c125d,
+      #010101,
+      #2930dd,
+      #61cffb,
+      #f7ffff,
+      #fefd55,
+      #e83c22,
+      #4a0b31,
+      #010101
+    );
+    background-blend-mode: soft-light;
+  }
+`;
+export const Gradient1 = styled(Background)`
+  &::before {
+    content: "";
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    width: ${(p) => p.size + "px"};
+    height: ${(p) => p.size + "px"};
+    transform: translate(-50%, -50%);
+
+    background-image: url("data:image/svg+xml,%3Csvg viewBox='0 0 700 700' xmlns='http://www.w3.org/2000/svg'%3E%3Cfilter id='noiseFilter'%3E%3CfeTurbulence type='fractalNoise' baseFrequency='0.65' numOctaves='3' stitchTiles='stitch'/%3E%3C/filter%3E%3Crect width='100%25' height='100%25' filter='url(%23noiseFilter)'/%3E%3C/svg%3E"),
+      url(https://img.freepik.com/free-photo/portrait-white-man-isolated_53876-40306.jpg);
+    background-size: cover;
+    background-position: center;
+
+    background-blend-mode: soft-light;
+  }
+`;
+export const Gradient39 = styled(Background)`
   background-image: url("data:image/svg+xml,%3Csvg viewBox='0 0 700 700' xmlns='http://www.w3.org/2000/svg'%3E%3Cfilter id='noiseFilter'%3E%3CfeTurbulence type='fractalNoise' baseFrequency='0.65' numOctaves='3' stitchTiles='stitch'/%3E%3C/filter%3E%3Crect width='100%25' height='100%25' filter='url(%23noiseFilter)'/%3E%3C/svg%3E"),
     conic-gradient(
       #010101,
@@ -76,38 +223,7 @@ const Wrapper = styled.div<{ sizeRatio: number }>`
       #4a0b31,
       #010101
     );
-  /* background: url(https://img.freepik.com/free-photo/portrait-white-man-isolated_53876-40306.jpg);
-  background-size: 600px 600px;
-  background-position: center; */
   background-blend-mode: soft-light;
-  --border-width: 0.5px;
-  --edge-width: 4px;
-  &::before,
-  &:after {
-    content: "";
-    position: absolute;
-    inset: 0;
-    border-radius: inherit;
-  }
-  &:before {
-    border: var(--border-width) solid black;
-  }
-  &:after {
-    background: linear-gradient(135deg, white, black);
-    --gradient-end: calc(
-      var(--radius) / 2 - var(--edge-width) - var(--border-width)
-    );
-    mask-image: radial-gradient(
-      circle at center,
-      transparent var(--gradient-end),
-      black var(--gradient-end)
-    );
-    mix-blend-mode: soft-light;
-  }
-  & > & {
-    transform: rotate(15deg) scale(0.7);
-    transform-origin: center;
-  }
 `;
 const DragHandle = styled.div`
   position: absolute;
@@ -116,8 +232,12 @@ const DragHandle = styled.div`
 `;
 type Props = {
   steps?: number;
+  BackgroundComponent: typeof Background;
 };
-const ChromadynamicaManipulable39 = ({ steps = 7 }: Props) => {
+const ChromadynamicaManipulable = ({
+  steps = 7,
+  BackgroundComponent,
+}: Props) => {
   const ref = React.useRef<HTMLDivElement>(null);
   const [angle, setAngle] = React.useState(0);
 
@@ -134,62 +254,70 @@ const ChromadynamicaManipulable39 = ({ steps = 7 }: Props) => {
     });
   }, []);
 
-  return (
-    <Center>
-      <svg aria-hidden="true">
-        <filter id="noiseFilter">
-          <feTurbulence
-            type="fractalNoise"
-            baseFrequency="6.29"
-            numOctaves="6"
-            stitchTiles="stitch"
-          ></feTurbulence>
-        </filter>
-        <filter id="noiseFilter2">
-          <feTurbulence
-            type="fractalNoise"
-            baseFrequency="0.65"
-            numOctaves="3"
-            stitchTiles="stitch"
-          />
-        </filter>
-      </svg>
+  const { ref: roRef, width: size } = useResizeObserver<HTMLDivElement>();
 
-      <Segment steps={7} angle={angle} />
+  return (
+    <Center ref={roRef}>
+      <Segment
+        steps={steps}
+        angle={angle}
+        size={size}
+        BackgroundComponent={BackgroundComponent}
+      />
       <DragHandle ref={ref} />
     </Center>
   );
 };
 
-export default ChromadynamicaManipulable39;
+export default ChromadynamicaManipulable;
 
 const Segment = ({
   steps,
   step = 0,
   children,
   angle: angleProp,
+  BackgroundComponent,
+  size,
   ...otherProps
-}: React.ComponentProps<typeof Wrapper> & {
+}: React.ComponentProps<typeof Wheel> & {
   step?: number;
   steps: number;
   angle?: number;
+  size: number;
+  BackgroundComponent: typeof Background;
 }) => {
   const parentAngle = useAngle();
   const [angle, setAngle] = React.useState(0);
+  const isStickingRef = React.useRef(false);
+  const [play] = useSound("https://ui.gallery/click_06.wav", {
+    forceSoundEnabled: true,
+    playbackRate: 8,
+  });
 
-  React.useEffect(() => {
+  React.useLayoutEffect(() => {
     if (parentAngle) {
       setAngle((angle) => {
         if (parentAngle >= angle + 15) {
+          if (!isStickingRef.current) {
+            play();
+          }
+          isStickingRef.current = true;
           return parentAngle - 15;
           //   console.log("SWAG");
         } else if (parentAngle - angle <= 0) {
+          if (!isStickingRef.current) {
+            play();
+          }
+          isStickingRef.current = true;
+
           return parentAngle;
         }
+        isStickingRef.current = false;
+
         return angle;
       });
     }
-  }, [parentAngle]);
+  }, [parentAngle, play]);
 
   const stepSize = 1 / steps;
   const sizeRatio = 1 - step * stepSize;
@@ -198,14 +326,29 @@ const Segment = ({
 
   return (
     <>
-      <Wrapper
-        style={{ transform: `rotate(${resultAngle}deg)` }}
+      <Wheel
         sizeRatio={sizeRatio}
-      />
+        style={{
+          overflow: step === 0 ? "hidden" : undefined,
+        }}
+      >
+        <BackgroundComponent
+          size={size}
+          style={{ transform: `rotate(${resultAngle}deg)` }}
+        />
+
+        {/* <Gradient style={{ transform: `rotate(${resultAngle}deg)` }} /> */}
+      </Wheel>
 
       {step < steps - 1 ? (
         <AngleContext.Provider value={resultAngle}>
-          <Segment steps={steps} step={step + 1} sizeRatio={sizeRatio} />
+          <Segment
+            steps={steps}
+            step={step + 1}
+            sizeRatio={sizeRatio}
+            size={size}
+            BackgroundComponent={BackgroundComponent}
+          />
         </AngleContext.Provider>
       ) : null}
     </>
--- components/toys/ChromadynamicaManipulable/index.tsx
+++ components/toys/ChromadynamicaManipulable/index.tsx
@@ -1,12 +1,28 @@
 import React from "react";
-import ChromadynamicaManipulable from "./ChromadynamicaManipulable";
-import { HStack } from "../../UI/Stack";
+import ChromadynamicaManipulable, {
+  Gradient39,
+  Gradient3,
+  Gradient1,
+  Gradient2,
+} from "./ChromadynamicaManipulable";
+import { HStack, VStack } from "../../UI/Stack";
+import DancingPeople from "./DancingPeople";
 
 const index = () => {
   return (
-    <HStack mb={64} mt={32} justifyContent="center">
-      <ChromadynamicaManipulable />
-    </HStack>
+    <VStack
+      gap={64}
+      mb={64}
+      mt={32}
+      justifyContent="center"
+      alignItems="center"
+    >
+      <ChromadynamicaManipulable BackgroundComponent={Gradient2} />
+      <ChromadynamicaManipulable BackgroundComponent={Gradient1} />
+      <ChromadynamicaManipulable BackgroundComponent={Gradient3} />
+      <ChromadynamicaManipulable BackgroundComponent={Gradient39} />
+      {/* <DancingPeople /> */}
+    </VStack>
   );
 };
 
